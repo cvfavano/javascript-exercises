5f523801714b3966b35fc823bdc03f4aad1df4a5
Christa Favano <crstav@gmail.com>
2022-08-05 23:42:36 -0400

    Add function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var reversedString = new Array();
 
+    for(var i = 0; i < string.length; i++) {
+        reversedString.unshift(string[i]);
+    }
+    return reversedString.join('');
 };
 
 // Do not edit below this line
