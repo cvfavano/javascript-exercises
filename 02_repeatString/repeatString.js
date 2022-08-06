184d52400e589d5194d9554666781e17427b099e
Christa Favano <crstav@gmail.com>
2022-08-05 22:39:46 -0400

    Complete exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	var repeatedString ='';
+	if(num < 0) {
+		return 'ERROR';
+	}
+	for (i = 0; i < num; i++) {
+		repeatedString += string;
+	}
+	return repeatedString;
 
 };
 
