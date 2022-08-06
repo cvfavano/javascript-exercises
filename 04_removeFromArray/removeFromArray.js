2707afef89d8ea64a4126c325726681a1dd4fdb5
Christa Favano <crstav@gmail.com>
2022-08-06 15:50:54 -0400

    Add working function to remove any number of arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+ function removeFromArray() {
+	//arguments[0]
+	//.filter(function(){
 
-};
-
+	if (arguments.length < 2) {
+		return 'ERROR';
+	}
+	//loop through the given argument/s
+	for(let i = 1; i<arguments.length; i++){
+		//loop through the array
+		for(j = 0; j < arguments[0].length; j++){
+			if(arguments[0][j] === arguments[i] ) {
+				arguments[0].splice(j,1);
+			
+			}
+		} 
+	}	
+	console.log(arguments[0])
+	return arguments[0];
+}
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
