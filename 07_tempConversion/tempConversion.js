70517386065b2de7378c530c234d0c2b60470c53
Christa Favano <crstav@gmail.com>
2022-08-06 22:00:03 -0400

    Add functions to convert fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  validateInput(num);
+  return Math.round((num-32)*(5/9) * 10) /10;
 };
 
-const ctof = function() {
-
+const ctof = function(num ) {
+  return  Math.round((num * (9/5) +32) * 10)/10;
 };
+
+function validateInput(num) {
+  if(typeof num === 'string' || isNaN(num)) {
+    return 'ERROR';
+  }
+}
 
 // Do not edit below this line
 module.exports = {
